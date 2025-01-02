e305ccfda8018f8ed7613c0d118f6022aecec3f0
lachi <lachi.dong360@gmail.com>
2025-01-03 00:12:36 +0200

    added other files

---
+++ 9 Forms and Data/9.3 Form Data in Deno KV/project/addressController.js
@@ -0,0 +1,23 @@
+import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
+import * as addressServices from "./addressService.js";
+
+
+const eta = new Eta({views:`${Deno.cwd()}/templates/`})
+
+
+const getAndRenderAddresses  =  async (c) => {
+  const addresses = await addressServices.getData();  // Await the result
+  return await c.html(eta.render("index.eta", { addresses }));  // Pass it correctly to the template
+}
+
+
+const listAndRenderAddresses = async (c) => {
+  const body = await c.req.parseBody();
+  console.log(body);
+  await addressServices.setData(body);  // Save the address data
+  const addresses = await addressServices.getData();  // Fetch updated addresses
+  return await c.html(eta.render("index.eta", { addresses }));  // Pass the updated addresses to the template
+}
+
+
+export {getAndRenderAddresses, listAndRenderAddresses}
---
+++ 9 Forms and Data/9.4 POST Redirect GET Pattern/Post redirect example/app.js
@@ -0,0 +1,13 @@
+import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+
+const app = new Hono();
+
+app.get("/", (c) => c.html(`
+<form method="POST" action="/">
+  <input type="submit"></input>
+</form>
+`));
+
+app.post("/", (c) => c.redirect("/"));
+
+Deno.serve(app.fetch);
