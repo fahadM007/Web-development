cea58a7988a4f01edea452848a17056ea65c693e
lachi <lachi.dong360@gmail.com>
2025-01-09 10:12:31 +0200

    starting crud oparations

---
+++ 10 CRUD/1.0 CRUD/app.js
@@ -0,0 +1,12 @@
+import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
+import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+
+const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
+
+const app = new Hono();
+
+app.get("/todos", (c) => {
+  return c.html(eta.render("todos.eta"))
+});
+
+export default app;
