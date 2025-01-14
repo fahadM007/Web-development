6308e5270b4116b04509ec06633eeb7cd2a6eb68
fahadM007 <fahad.mohamed@centria.fi>
2025-01-14 05:52:06 +0200

    done with books

---
+++ 10 CRUD/1.0 crud/Assignment/app.js
@@ -0,0 +1,12 @@
+import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+import * as bookController from "./bookController.js";
+
+const app = new Hono();
+
+app.get("/books", bookController.showForm);
+app.get("/books/:id", bookController.showBook);
+app.post("/books", bookController.createBook);
+app.post("/books/:id", bookController.updateBook);
+app.post("/books/:id/delete", bookController.deleteBook);
+
+export default app;
